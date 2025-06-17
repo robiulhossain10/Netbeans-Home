d0b53ace9afe8d085d7df3d07d8e5105ceaa3586
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-17 21:03:26 +0600

    hi

---
+++ src/main/java/com/methods/methodexample/MethodExample.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.methods.methodexample;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class MethodExample {
+
+    public static void main(String[] args) {
+        MethodExample obj = new MethodExample();
+        obj.printName();
+        obj.printName("Ahmad");
+        obj.printName();
+    }
+    
+    String name;
+    int age;
+    
+    void printName(){
+        
+        System.out.println("Hello " + name + "!!");
+    }
+    
+    void printName(String nameParam){
+        name = nameParam;
+        System.out.println("Hello " + name + "!!");
+    }
+    
+    
+    
+    
+    
+    
+    
+}
