6592dedf7b397b91773d351369b5d0a3f17bda8e
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-21 23:37:16 +0600

    first_commit

---
+++ src/main/java/com/methods/methodexample/ConstructorMethod.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class ConstructorMethod {
+    public static void main(String[] args) {
+        ConstructorMethod std1 = new ConstructorMethod(" Robiul" ,26);
+        ConstructorMethod std2 = new ConstructorMethod(" Jibon",26);
+    }
+    
+    
+    
+    public ConstructorMethod(String name, int age){
+        System.out.println("This is Constructor Method" + name);
+    }
+    
+    
+}
