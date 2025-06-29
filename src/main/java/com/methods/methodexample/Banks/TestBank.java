26603e0c7717877bb527d2b4dc8ec345bbe7dd06
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-29 23:24:46 +0600

    AssociationBank

--- src/main/java/com/methods/methodexample/Banks/TestBank.java
+++ src/main/java/com/methods/methodexample/Banks/TestBank.java
@@ -13,12 +13,12 @@ public class TestBank {
         BankAccounts bank1 = new BankAccounts(1232555455l, "Robiul Hossain", 70000,0.05);
         BankAccounts bank2 = new BankAccounts(1232555455l, "Kazi Joynal", 50000,0.00);
         BankAccounts bank3 = new BankAccounts(1232555455l, "Habib Khan", 500,0.00);
-        BankAccounts bank4 = new BankAccounts(1232555455l, "Fahim Hossain", 20000,0.00);
+        BankAccounts bank4 = new BankAccounts(1232555455l, "Sabrina Aurpa", 100000,0.00);
 
         
-        bank1.accountAccess();
-        bank1.bankLoan(10000);
-        bank1.loanRepayment(1000,2);
+        bank4.accountAccess();
+//        bank1.bankLoan(10000);
+//        bank1.loanRepayment(1000,2);
      
 //        bank2.accountAccess();
 //        bank2.accountAccess();
