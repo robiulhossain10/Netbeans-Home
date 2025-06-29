ebe96f85dfcf9b7cb9d0c54831ba218ec54e0c9d
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-29 23:43:48 +0600

    Aggregations

---
+++ src/main/java/com/methods/methodexample/Aggregations/Teacher.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.Aggregations;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class Teacher {
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
