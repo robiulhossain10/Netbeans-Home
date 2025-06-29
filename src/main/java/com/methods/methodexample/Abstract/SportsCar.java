5c3831ad771dd462e45266ddc7b08ed393dad67a
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-29 19:58:33 +0600

    abstractClass

---
+++ src/main/java/com/methods/methodexample/Abstract/SportsCar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.Abstract;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class SportsCar extends Car{
+
+    @Override
+    void start() {
+        System.out.println("Sports Car is Start");
+    }
+
+    @Override
+    void drive() {
+       System.out.println("Sports Car is Drive");
+    }
+    
+}
