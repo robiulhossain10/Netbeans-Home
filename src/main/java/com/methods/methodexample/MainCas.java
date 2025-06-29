79808747911019e29e61cf0d6152115866825298
Robiul Hossain <robiulhossain0110@gmail.com>
2025-06-29 20:01:16 +0600

    Primitive

---
+++ src/main/java/com/methods/methodexample/MainCas.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class MainCas {
+    public static void main(String[] args) {
+        byte b = 127;
+        Byte b2 = -125;
+        Character c2 = 'A'; //wrapper class
+        Short s = 1000;
+        Long l = 123456789L;
+        Float f = 5.75f;
+        Double d = 19.99d;
+        Boolean bo = true;
+        System.out.println("byte: "+ b);
+        System.out.println("Byte: "+ b2);
+        System.out.println("Character: "+ c2);
+        System.out.println("Short: "+ s);
+        System.out.println("Long: "+ l);
+        System.out.println("Float: "+ f);
+        System.out.println("Double: "+ d);
+        System.out.println("Boolean: "+ bo);
+        
+       
+        
+    }
+}
