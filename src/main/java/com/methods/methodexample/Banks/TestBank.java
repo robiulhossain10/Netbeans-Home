738dd305aa26de962015dc4ebfd35e00ddcf9036
Robiul Hossain <robiulhossain0110@gmail.com>
2025-07-01 23:19:35 +0600

    Bank

--- src/main/java/com/methods/methodexample/Banks/TestBank.java
+++ src/main/java/com/methods/methodexample/Banks/TestBank.java
@@ -9,17 +9,18 @@ package com.methods.methodexample.Banks;
  * @author ROBIUL
  */
 public class TestBank {
+    
     public static void main(String[] args) {
-        BankAccounts bank1 = new BankAccounts(1232555455l, "Robiul Hossain", 70000,0.05);
-        BankAccounts bank2 = new BankAccounts(1232555455l, "Kazi Joynal", 50000,0.00);
-        BankAccounts bank3 = new BankAccounts(1232555455l, "Habib Khan", 500,0.00);
-        BankAccounts bank4 = new BankAccounts(1232555455l, "Sabrina Aurpa", 100000,0.00);
-
+        BankAccounts bank1 = new BankAccounts(1232555455l, "Robiul Hossain", 70000, 2135);
+        BankAccounts bank2 = new BankAccounts(1232555455l, "Kazi Joynal", 50000, 5458);
+        BankAccounts bank3 = new BankAccounts(1232555455l, "Habib Khan", 500, 7854);
+        BankAccounts bank4 = new BankAccounts(1232555455l, "Sabrina Aurpa", 100000, 7547);
         
         bank4.accountAccess();
-//        bank1.bankLoan(10000);
+        bank4.bankLoan(10000, 1);
+        bank4.recharge(100);
 //        bank1.loanRepayment(1000,2);
-     
+
 //        bank2.accountAccess();
 //        bank2.accountAccess();
 //        bank3.accountAccess();
