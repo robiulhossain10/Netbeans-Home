8cf300a154e3397f42c29a12609ad7f4d8f2974c
Robiul Hossain <robiulhossain0110@gmail.com>
2025-07-04 11:41:25 +0600

    MathCal

---
+++ src/main/java/com/methods/methodexample/BinaryIO/Mains.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.BinaryIO;
+
+/**
+ *
+ * @author ROBIUL
+ */
+public class Mains {
+    public static void main(String[] args) {
+        MathCalculate ob = new MathCalculate();
+        
+//        ob.calculateSum(20+30);
+//        ob.calculateMulti();
+        ob.calculateMinus(10-2);
+//        ob.calculateDivisions();
+    }
+}
